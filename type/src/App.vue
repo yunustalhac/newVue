aca12b85cc543267ed828eba40d550e844000a83
yunustalhac <yunustalhacoban@gmail.com>
2023-12-13 11:32:36 +0300

    bismillah

---
+++ type/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>bismillah</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue" ;
+
+const name = ref("lkwar")
+
+name.value = "2"
+</script>
